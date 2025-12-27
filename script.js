b2a0c93c39ca7e599b6f0c826b36a70ad5cd538f
saycheese2k7 <241291587+saycheese2k7@users.noreply.github.com>
2025-12-27 17:03:14 +0600

    value retrieval working

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function newbook(t, a, rt){
     this.title =  t;
     this.author = a;
-    this.year = rt;
+    this.timeOfRead = rt;
 }
 
+let title, auth, time;
+
+function addbook(){
+    title = document.getElementById("title").value;
+    auth = document.getElementById("author").value; 
+    time = document.getElementById("time").value;
+    return [title, auth, time];
+}
+
+let tit, aut, tim;
+
+function test(){
+    [tit, aut, tim] = addbook();
+    console.log(tit); 
+}
+
+function test2(){
+    console.log(tit);
+}
+
+
+
+/*
+function test(){
+    let title = document.getElementById('title').value;
+    console.log(title)
+};*/
